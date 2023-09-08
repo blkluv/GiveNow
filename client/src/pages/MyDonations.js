73bfd03defdfb144d0817344950b5b9f671623b1
lacnoskillz <kaikane3457@hotmail.com>
2023-09-07 21:01:35 -1000

    added simple turnary for displaying differnt messages if user is top doner or not. plan on making badge/image instead of message

--- client/src/pages/MyDonations.js
+++ client/src/pages/MyDonations.js
@@ -50,7 +50,8 @@ const MyDonations = () => {
             <h2>userinfo</h2>
             <p>username: {data.me.username}</p>
             <p>email: {data.me.email}</p>
-            <p>topdoner: {data.me.topdoner.toString()}</p>
+            <p>topdoner: {data.me.topdoner == true ? "You are a Top Donor!" : "Not"}</p>
+
           </div>
           <div>
             <h2>Donations</h2>
