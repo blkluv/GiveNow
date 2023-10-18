7a07fbd869c1706363538f90c160e36a4197811e
lacnoskillz <kaikane3457@hotmail.com>
2023-10-17 23:59:29 -0500

    added responsiveness to homepage hero. added homepage css.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,14 +3,16 @@ import Carousel from 'react-bootstrap/Carousel';
 import { GET_ORGANIZATIONS } from "../utils/queries";
 import { useQuery } from '@apollo/client';
 import { useNavigate } from "react-router-dom";
-
+import '../components/styles/home.css'
 const styles = {
     styles: {
-        backgroundColor: "rgba(0, 0, 0, 0.6)"
+        backgroundColor: "rgba(0, 0, 0, 0.6)",
+      
+
     },
     background: {
         backgroundImage: "url('https://mdbcdn.b-cdn.net/img/new/slides/041.webp')",
-    height: "50vh",
+    height: "60vh",
     backgroundSize: "cover", 
     backgroundRepeat: "no-repeat",
     },
@@ -20,7 +22,8 @@ const styles = {
     },
     dasha: {
       display: 'flex',
-      
+      marginTop: "2%",
+      marginBottom: "2%",
       flexDirection: 'column',
       alignItems: 'center'
     },
@@ -65,9 +68,9 @@ return(
     <div className="p-5 text-center bg-image rounded-3" style={styles.background}>
   <div className="mask" style={styles.styles}>
     <div className="d-flex justify-content-center align-items-center h-100">
-      <div className="text-white">
-        <h1 className ="mb-3">Join Us in Making a Difference Through Compassionate Giving</h1>
-        <h4 className="mb-3">Find and support a charity that aligns with your passions.</h4>
+      <div className="text-white herotext">
+        <h1 className ="mb-3 heroh1">Join Us in Making a Difference Through Compassionate Giving</h1>
+        <h4 className="mb-3 heroh4">Find and support a charity that aligns with your passions.</h4>
         <a className="btn btn-outline-light btn-lg" href="/donate" role="button">GiveNow</a>
       </div>
     </div>
