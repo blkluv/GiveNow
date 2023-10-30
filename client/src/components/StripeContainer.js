c8a234b443521e4965808be6d67c7f4dc9d83a5d
lacnoskillz <kaikane3457@hotmail.com>
2023-10-30 15:29:44 -0500

    commit on laptop

--- client/src/components/StripeContainer.js
+++ client/src/components/StripeContainer.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Elements } from "@stripe/react-stripe-js"
 import { loadStripe } from "@stripe/stripe-js";
 import PaymentForm from "./PaymentForm";
-const PUBLIC_KEY = "pk_test_51NkBSxGYCFpESPA0P4JxTwGu8VjHQvDPNEy0eyZaDGf4uJ4LFtYiP4E6k5WnA92FxKPPO98ip5TyD9NClvgSF1al00rLVkFdIW"
+const PUBLIC_KEY = process.env.REACT_APP_STRIPE_PUBLIC_TEST
 
 const stripeTestPromise = loadStripe(PUBLIC_KEY)
 
